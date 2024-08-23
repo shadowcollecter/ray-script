bb76d6b994fb813011dca3d7589138b8082e9e38
Shadopwcollecter <shadowcollect@gmail.com>
2024-08-23 19:27:48 +0800

    chore: Remove commented out code and unused imports in script.py

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
-import torch
-from ray.train import ScalingConfig
-from ray.train.torch import TorchTrainer
-from vllm import LLM
+# import torch
+# from ray.train import ScalingConfig
+# from ray.train.torch import TorchTrainer
+# from vllm import LLM
 
-def train_func():
-    llm = LLM('facebook/opt-6.7b', tensor_parallel_size=2, gpu_memory_utilization=0.8)
-    output = llm.generate('San Francisco is a')
-    print(output)
+# def train_func():
+#     llm = LLM('facebook/opt-6.7b', tensor_parallel_size=2, gpu_memory_utilization=0.8)
+#     output = llm.generate('San Francisco is a')
+#     print(output)
     
 
-trainer = TorchTrainer(train_func, 
-                       scaling_config=ScalingConfig(
-                            num_workers=2, 
-                            use_gpu=True, 
-                            resources_per_worker={'GPU': 1}
-                            )
-                       )
+# trainer = TorchTrainer(train_func, 
+#                        scaling_config=ScalingConfig(
+#                             num_workers=2, 
+#                             use_gpu=True, 
+#                             resources_per_worker={'GPU': 1}
+#                             )
+#                        )
 
-trainer.fit()
+# trainer.fit()
+
 
 
 # import ray
@@ -34,3 +35,21 @@ trainer.fit()
 # # The following command will block until two Ray pods with GPU access are scaled
 # # up and the actors are placed.
 # ray.get([actor.say_hello.remote() for actor in gpu_actors])
+
+import ray
+from vllm import LLM
+
+ray.init()
+
+@ray.remote(num_gpus=2)
+class LLMActor:
+    def __init__(self):
+        self.llm = LLM('facebook/opt-6.7b', tensor_parallel_size=2, gpu_memory_utilization=0.8)
+    
+    def generate(self, text):
+        return self.llm.generate(text)
+    
+
+# run an actor
+llm_actor = LLMActor.remote()
+output = ray.get(llm_actor.generate.remote('San Francisco is a'))
